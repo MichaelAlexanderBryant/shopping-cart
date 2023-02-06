4d38171cc9a474cfe0ca410e8ebc7da87f5ff46e
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-06 10:09:37 -0800

    Add navigation bar items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import HomePage from './pages/HomePage';
 import ShopPage from './pages/ShopPage';
 
 function App() {
   return (
     <div className="App">
-        <ShopPage />
+        <HomePage />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,15 @@
 function NavBar() {
     
     return (
-        <div>
+        <div id="header-nav">
             <h1>Boltzmann's Books</h1>
+            <div id="navigation">
+                <ul id="nav-list">
+                    <li>Home</li>
+                    <li>Books</li>
+                    <li>Shopping Cart</li>
+                </ul>
+            </div>
         </div>
     )
 }
