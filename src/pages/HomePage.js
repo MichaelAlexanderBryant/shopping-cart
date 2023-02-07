6f5229523c373c52cecf40c208f194ebfe0807fa
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-06 19:27:34 -0800

    Style shopping cart number

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,16 +3,14 @@ import boltzmann from '../assets/boltzmann.jpg';
 function HomePage() {
 
     return (
-        <div>
-            <div id="homepage">
-                <img src={boltzmann} alt="boltzmann"/>
-                <div id="homepage-intro">
-                    <p>Welcome to Boltzmann's Books!</p>
-                    <p>A specialty bookstore for statistical mechanics.</p>
-                    <p>Ludwig Boltzmann, who spent much of his life studying statistical mechanics, died in 1906, by his own hand.
-                        Paul Ehrenfest, carrying on the work, died similarly in 1933. Now it is <strong>your</strong> turn to study statistical mechanics.
-                    </p>
-                </div>
+        <div id="homepage">
+            <img src={boltzmann} alt="boltzmann"/>
+            <div id="homepage-intro">
+                <p>Welcome to Boltzmann's Books!</p>
+                <p>A specialty bookstore for statistical mechanics.</p>
+                <p>Ludwig Boltzmann, who spent much of his life studying statistical mechanics, died in 1906, by his own hand.
+                    Paul Ehrenfest, carrying on the work, died similarly in 1933. Now it is <strong>your</strong> turn to study statistical mechanics.
+                </p>
             </div>
         </div>
     )
