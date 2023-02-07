5a8f561a401f8d883c4797b49bf69a356ab726b6
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-07 08:42:07 -0800

    Add hash map to count shopping cart items

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,5 +1,20 @@
 function ShoppingCart(props) {
 
+    let counts = {};
+    for (let i = 0; i < props.booksInCart.length; i++) {
+        if (Object.keys(counts).includes(props.booksInCart[i])) {
+            counts[props.booksInCart[i]] += 1;
+        } else {
+            counts[props.booksInCart[i]] = 1;
+        };
+    };
+
+    return ( 
+        <div>
+            {Object.keys(counts).map((item) =>
+                <div key={item}>{item + ": " + counts[item]}</div>)}
+        </div>)
+
 }
 
 export default ShoppingCart;
